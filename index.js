39cd1fcc2be616d993df76e477db0d7d488e0996
Meesam <70945820+meesam4687@users.noreply.github.com>
2024-03-11 18:08:24 +0000

    Use MongoDB instead of Saving to JSON, Dont Use Local Images

--- index.js
+++ index.js
@@ -1,37 +1,24 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const articleJSON = require("./articles/articles.json")
 app.use(express.static('public'));
 require("dotenv").config()
-let PORT = process.env.PORT
+let PORT = process.env.PORT || 8080
 const cors = require('cors')
+const quickmongo = require('quickmongo');
+const db = new quickmongo.Database(process.env.DBURL);
+
+db.on("ready", async () => {
+  console.log("[QuickMongo] Connected to the Database.");
+});
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/articles", async (req, res) => {
-  res.send(articleJSON)
+  let articles = await db.get('articles');
+  res.send(articles)
 })
 
-let accounts = [
-  {
-    "username": "meesam4687",
-    "password": process.env.PASS_MEESAM
-  }
-]
-
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  let authObj = {
-    "username": username,
-    "password": password
-  }
-  const isAuthenticated = accounts.some(obj => obj.username === authObj.username && obj.password === authObj.password)
-  res.json({ isAuthenticated });
-});
-
-app.use('/articles/assets', express.static('articles/assets'));
-
-app.listen(PORT, console.log("Listening to port " + PORT))
+app.listen(PORT, console.log("[Express] Listening to port " + PORT))
